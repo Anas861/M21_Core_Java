1e24582f4b2b9f36cb5fc0f9b2a667f607bb9a3a
Admin <Admin@DESKTOP-HH566JI>
2022-07-12 22:28:18 +0530

    First Commit

---
+++ Day2_Intro_to_Java/src/com/cg/java/Program1.java
@@ -0,0 +1,16 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int a=15;
+		Scanner s1=new Scanner(System.in);
+		int a=s1.nextInt();
+		System.out.println("The value of a is:"+a);
+		s1.close();
+		}
+
+}
