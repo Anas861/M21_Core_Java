d3188bff3d86812e2f2f3370bf1d3ab9cbfe99e2
Admin <Admin@DESKTOP-HH566JI>
2022-07-19 12:47:20 +0530

    Second Commit

---
+++ Day6_Abstract,final and super keyword/src/com/cg/superkeyword/SuperMethod.java
@@ -0,0 +1,32 @@
+package com.cg.superkeyword;
+//Super keyword:- it is used to access parent class method
+class Animal
+{
+	public int a=10,b=20;
+	void display()
+	{
+		System.out.println("The addition of a and b is:"+(a+b) );
+	}
+}
+class Tiger extends Animal
+{
+	void display()
+	{
+		System.out.println("Tiger");
+	}
+	void print()
+	{
+		display();
+		//accessing the parent class method using super keyword
+		super.display();
+	}
+}
+public class SuperMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//obj.creation
+		Tiger t=new Tiger();
+		t.print();
+	}
+}
