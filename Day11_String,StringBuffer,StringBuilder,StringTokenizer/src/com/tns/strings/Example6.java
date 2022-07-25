78599d842fda73370d6341061d6577c2957a0de0
Admin <Admin@DESKTOP-HH566JI>
2022-07-25 15:04:53 +0530

    First Commit

---
+++ Day11_String,StringBuffer,StringBuilder,StringTokenizer/src/com/tns/strings/Example6.java
@@ -0,0 +1,14 @@
+package com.tns.strings;
+
+import java.util.StringTokenizer;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringTokenizer st=new StringTokenizer("Lets try this");
+		System.out.println(st.nextToken());
+		System.out.println(st.nextToken());
+	}
+
+}
